a378ea4b952f1dc543f06a785edd67f736e5a56d
powellanalytics <dylan@powellanalytics.com>
2023-12-16 15:20:03 -0500

    finished learning A3 -- time to start building analytics views

--- utils/clean_column_names.py
+++ utils/clean_column_names.py
@@ -1,37 +1,43 @@
 #import pandas as pd
 import re
 
-def clean_column_names(df):
+def clean_names(name):
     """
-    Clean the column names of a pandas DataFrame for safe use in databases.
+    Clean a given name (column or index name) for safe use in databases.
     - Convert to lowercase.
     - Replace spaces and special characters with underscores.
-    - Ensure column names do not start with numbers.
-    - Handle columns with names that are entirely numeric.
+    - Ensure the name does not start with numbers.
+    - Handle names that are entirely numeric.
     """
-    clean_names = []
-    for col in df.columns:
-        # Replace spaces and special characters with underscores
-        clean_name = re.sub(r'\W+', '_', col)
+    # Replace spaces and special characters with underscores
+    clean_name = re.sub(r'\W+', '_', name)
 
-        # Convert to lowercase
-        clean_name = clean_name.upper()
+    # Convert to lowercase
+    clean_name = clean_name.upper()
 
-        # Ensure it doesn't start with a number
-        if re.match(r'^\d', clean_name):
-            clean_name = f'col_{clean_name}'
+    # Ensure it doesn't start with a number
+    if re.match(r'^\d', clean_name):
+        clean_name = f'COL_{clean_name}'
 
-        # Handle entirely numeric column names
-        if clean_name.isdigit():
-            clean_name = f'col_{clean_name}'
+    # Handle entirely numeric names
+    if clean_name.isdigit():
+        clean_name = f'COL_{clean_name}'
 
-        clean_names.append(clean_name)
+    return clean_name
 
-    # Assign cleaned names back to the DataFrame's columns
-    df.columns = clean_names
+def clean_column_and_index_names(df):
+    """
+    Clean the column and index names of a pandas DataFrame for safe use in databases.
+    """
+    # Clean column names
+    df.columns = [clean_names(col) for col in df.columns]
+
+    # Clean index name
+    if df.index.name:
+        df.index.name = clean_names(df.index.name)
 
     return df
 
 # Example Usage
 # df = pd.DataFrame({...}) # Your DataFrame
-# df = clean_column_names(df)
+# df = clean_column_and_index_names(df)
